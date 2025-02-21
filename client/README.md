2e9f237ccbbd7d780644bb0026c6eaa4c77178ae
Raydberg Gabriel Chuquival Gil <raydga_2005@hotmail.com>
2025-02-20 21:10:50 -0500

    Add changes

---
+++ client/README.md
@@ -0,0 +1,14 @@
+# React + TypeScript + Vite
+
+# Instalar las dependencias
+```bash
+bun i
+```
+# Iniciar el servidor de desarrollo
+```bash
+bun run dev
+```
+# Agregar componentes de shadcn
+```bash
+bun x --bun shadcn@canary add button
+```
---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react/jsx-runtime";
+import { Button } from "./components/ui/button";
+
+export default function App() {
+  <Fragment>
+    <h1>Hola Mundo</h1>
+    <Button variant="outline">
+      Hola Mundo
+    </Button>
+  </Fragment>
+}
+
