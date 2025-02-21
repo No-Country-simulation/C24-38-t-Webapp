105e13067ae702fb60178f460fb40c3be2631c85
Raydberg Gabriel Chuquival Gil <raydga_2005@hotmail.com>
2025-02-20 21:14:25 -0500

    Add changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,13 @@ import { Fragment } from "react/jsx-runtime";
 import { Button } from "./components/ui/button";
 
 export default function App() {
-  <Fragment>
-    <h1>Hola Mundo</h1>
-    <Button variant="outline">
-      Hola Mundo
-    </Button>
-  </Fragment>
+  return (
+    <Fragment>
+      <h1>Hola Mundo</h1>
+      <Button variant="outline">
+        Hola Mundo
+      </Button>
+    </Fragment>
+  )
 }
 
