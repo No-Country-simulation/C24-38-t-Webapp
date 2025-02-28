a0ebd1f643169cede020706fafdc3c3bdf426928
Marto917 <martomdds@gmail.com>
2025-02-27 21:57:41 -0300

    add responsive menu

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,20 @@
+import { Routes, Route, useLocation } from "react-router-dom";
 import AuthScreen from "./pages/AuthScreen";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import WelcomeScreen from "./pages/WelcomeScreen";
-import { Routes, Route } from "react-router-dom";
+import Menu from "./components/Menu";
 
 export default function App() {
+  const location = useLocation();
+
+  const noMenuRoutes = ["/login", "/register", ""];
+
+  const showMenu = !noMenuRoutes.includes(location.pathname);
+
   return (
     <>
+      {showMenu && <Menu />}
       <Routes>
         <Route path="/" element={<WelcomeScreen />} />
         <Route path="/auth" element={<AuthScreen />} />
