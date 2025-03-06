a7848b335d458b622c80fef0800ef5072110b49d
SergioFraga <sergiozfraga@gmail.com>
2025-03-06 14:03:35 -0800

    Add resource /medicos/listarPorNombre

--- backend/src/main/java/com/equipo_c24_38/backend/service/MedicoService.java
+++ backend/src/main/java/com/equipo_c24_38/backend/service/MedicoService.java
@@ -6,30 +6,43 @@ import com.equipo_c24_38.backend.model.entity.Medico;
 import com.equipo_c24_38.backend.model.entity.Usuario;
 import com.equipo_c24_38.backend.model.tiposDatos.TipoUsuario;
 import com.equipo_c24_38.backend.repository.MedicoRepository;
+import com.equipo_c24_38.backend.repository.PacienteRepository;
 import com.equipo_c24_38.backend.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class MedicoService {
 
-  @Autowired
-  private MedicoRepository medicoRepository;
+    @Autowired
+    private MedicoRepository medicoRepository;
 
-  @Autowired
-  private UsuarioRepository usuarioRepository;
+    @Autowired
+    private UsuarioRepository usuarioRepository;
 
-  public Medico registrarMedico(DatosRegistroMedico datosRegistroMedico, Long idUsuario) {
-    Usuario usuario = usuarioRepository.findById(idUsuario)
-        .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+    private final PacienteRepository pacienteRepository;
 
-    if (usuario.getTipoUsuario() == TipoUsuario.MEDICO) {
-      Medico medico = new Medico(datosRegistroMedico, usuario);
-
-      return medicoRepository.save(medico);
-    } else {
-      throw new TipoUsuarioInvalidoException("El usuario no es de tipo MEDICO");
+    public MedicoService(PacienteRepository pacienteRepository) {
+        this.pacienteRepository = pacienteRepository;
     }
 
-  }
+    public Medico registrarMedico(DatosRegistroMedico datosRegistroMedico, Long idUsuario) {
+      Usuario usuario = usuarioRepository.findById(idUsuario)
+          .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+
+      if (usuario.getTipoUsuario() == TipoUsuario.MEDICO) {
+        Medico medico = new Medico(datosRegistroMedico, usuario);
+
+        return medicoRepository.save(medico);
+      } else {
+        throw new TipoUsuarioInvalidoException("El usuario no es de tipo MEDICO");
+      }
+
+    }
+
+    public List<String> listarMedicoPorNombre() {
+      return medicoRepository.findAllNombres();
+    }
 }
